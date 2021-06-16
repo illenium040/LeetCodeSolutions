89c28ea84ab7f499af8c0c943b85f2e03c8b0726
illenium <illenium040@mail.ru>
2021-06-16 23:41:48 +0300

    Добавьте файлы проекта.

---
+++ Easy/35-70/TwoSumSolution.cs
@@ -0,0 +1,23 @@
+﻿namespace LeetCodeSolutions.Easy._35_70
+{
+    /// <summary>
+    /// Algorithm results
+    //  Runtime  |  Memory | % best
+    //  452 ms   |  29 MB  | 11
+    /// </summary>
+    class TwoSumSolution
+    {
+        public static int[] TwoSum(int[] nums, int target)
+        {
+            for (int i = 0; i < nums.Length; i++)
+            {
+                for (int j = i + 1; j <= nums.Length - 1; j++)
+                {
+                    if (nums[i] + nums[j] == target)
+                        return new int[] { i, j };
+                }
+            }
+            return null;
+        }
+    }
+}
---
+++ Medium/35-70/AddTwoNumbersSolution.cs
@@ -0,0 +1,55 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace LeetCodeSolutions.Medium._35_70
+{
+    /// <summary>
+    /// Algorithm results
+    //  Runtime  |  Memory   | % best runtime | % best memory
+    //  104 ms   |  25.7 MB  | 11             | 99.81
+    /// </summary>
+    class AddTwoNumbersSolution
+    {
+        public class ListNode
+        {
+            public int val;
+            public ListNode next;
+            public ListNode(int x) { val = x; }
+        }   
+
+        public static ListNode AddTwoNumbers(ListNode l1, ListNode l2)
+        {
+            ListNode result;
+            int num = l1.val + l2.val;
+            if (num < 10)
+            {
+                result = new ListNode(num);
+                CreateNew(l1.next, l2.next, result, 0);
+            }
+            else
+            {
+                result = new ListNode(num % 10);
+                CreateNew(l1.next, l2.next, result, 1);
+            }
+            return result;
+        }
+        private static void CreateNew(ListNode l1, ListNode l2, ListNode res, int plus)
+        {
+            if (l1 != null || l2 != null || plus > 0)
+            {
+                var sum = (l1 == null ? 0 : l1.val) + (l2 == null ? 0 : l2.val) + plus;
+                if (sum < 10)
+                {
+                    res.next = new ListNode(sum);
+                    CreateNew(l1?.next, l2?.next, res.next, 0);
+                }
+                else
+                {
+                    res.next = new ListNode(sum % 10);
+                    CreateNew(l1?.next, l2?.next, res.next, 1);
+                }
+            }
+        }
+    }
+}
